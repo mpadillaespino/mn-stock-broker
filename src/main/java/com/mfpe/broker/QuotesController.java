125b1b6316d33a719978b51bfa8ddec31d908d4c
Miguel Fernando Padilla Espino <miguelpadillaespino@gmail.com>
2021-02-21 22:21:14 -0500

    Custom error HTTP response with tests

--- src/main/java/com/mfpe/broker/QuotesController.java
+++ src/main/java/com/mfpe/broker/QuotesController.java
@@ -1,8 +1,10 @@
 package com.mfpe.broker;
 
+import com.mfpe.broker.error.CustomError;
 import com.mfpe.broker.model.Quote;
 import com.mfpe.broker.store.InMemoryStore;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
@@ -21,6 +23,16 @@ public class QuotesController {
     @Get("/{symbol}")
     public HttpResponse getQuote(@PathVariable String symbol) {
         Optional<Quote> maybeQuote = store.fetchQuote(symbol);
+        if(maybeQuote.isEmpty()){
+            CustomError notFound = CustomError
+                    .builder()
+                    .status(HttpStatus.NOT_FOUND.getCode())
+                    .error(HttpStatus.NOT_FOUND.name())
+                    .message("quote symbol not available")
+                    .path("/quotes/" + symbol)
+                    .build();
+            return HttpResponse.notFound(notFound);
+        }
         return HttpResponse.ok(maybeQuote.get());
     }
 
--- src/test/java/com/mfpe/broker/QuotesControllerTest.java
+++ src/test/java/com/mfpe/broker/QuotesControllerTest.java
@@ -1,11 +1,14 @@
 package com.mfpe.broker;
 
+import com.mfpe.broker.error.CustomError;
 import com.mfpe.broker.model.Quote;
 import com.mfpe.broker.model.Symbol;
 import com.mfpe.broker.store.InMemoryStore;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
@@ -14,9 +17,12 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @MicronautTest
@@ -49,6 +55,22 @@ class QuotesControllerTest {
         Quote result = client.toBlocking().retrieve(HttpRequest.GET("/quotes/APPLE"), Quote.class);
         LOG.info("Result: {}", result);
         assertThat(apple).isEqualToComparingFieldByField(result);
+    }
+
+    @Test
+    void returnNotFoundOrUnsupportedSymbolTest(){
+        try {
+            client.toBlocking().retrieve(HttpRequest.GET("/quotes/UNSUPPORTED"));
+        } catch (HttpClientResponseException e) {
+            LOG.info("Body: {}", e.getResponse().getBody());
+            assertEquals(HttpStatus.NOT_FOUND, e.getResponse().getStatus());
+            Optional<CustomError> customError = e.getResponse().getBody(CustomError.class);
+            assertTrue(customError.isPresent());
+            assertEquals(404, customError.get().getStatus());
+            assertEquals("NOT_FOUND", customError.get().getError());
+            assertEquals("quote symbol not available", customError.get().getMessage());
+            assertEquals("/quotes/UNSUPPORTED", customError.get().getPath());
+        }
     }
 
     private BigDecimal randomValue() {
